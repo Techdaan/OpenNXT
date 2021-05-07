158eb7b63f9fd33481630c04cf47f82d290ec2e5
woahscam <elkin.austin@hotmail.com>
2021-05-07 02:37:36 -0400

    Update constants to support texture dumper paths

--- src/main/kotlin/com/opennxt/Constants.kt
+++ src/main/kotlin/com/opennxt/Constants.kt
@@ -10,4 +10,8 @@ object Constants {
     val CACHE_PATH = DATA_PATH.resolve("cache")
     val PROT_PATH = DATA_PATH.resolve("prot")
     val RESOURCE_PATH = DATA_PATH.resolve("resources")
+    val TEXTURES_PATH_DXT = DATA_PATH.resolve("textures/dxt")
+    val TEXTURES_PATH_PNG = DATA_PATH.resolve("textures/png")
+    val TEXTURES_PATH_MIP = DATA_PATH.resolve("textures/mipmap")
+    val TEXTURES_PATH_ETC = DATA_PATH.resolve("textures/etc")
 }
